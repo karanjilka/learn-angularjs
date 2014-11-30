be7158ad1ee3b55bae3b8ea856dc7a4f8963f245
karan <karan4230@gmail.com>
2014-11-30 12:39:37 +0530

    first commit

---
+++ directives/ratting/js/rating.js
@@ -0,0 +1,16 @@
+(function() {
+    var app = angular.module("rateApp",[]);
+
+    app.directive('rating', function() {
+        return {
+            restrict: 'A',
+            templateUrl:'rate-template.html',
+            scope:{
+
+            }
+            link: function(scope, elem, attrs) {
+                //console.log("Recognized the fundoo-rating directive usage");
+            }
+        };
+    });
+})();
